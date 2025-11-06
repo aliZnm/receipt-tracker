cfac99b6fc5a06c1a3af3952fd68d938e78ee80b
Abdulrahman Ali <abdul76985@gmail.com>
2025-11-06 17:35:56 -0500

    Add AddReceiptForm component and render it in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-
+import AddReceiptForm from "./components/AddReceiptForm"
 
 function App(){
-  
+  return(
+    <div>
+      <h1>Receipt Tracker</h1>
+      <AddReceiptForm />
+    </div>
+  );
 }
 
 export default App
---
+++ src/components/AddReceiptForm.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+export default function AddReceiptForm( { onAddReceipt }){
+    const [store, setStore] = useState("");
+    const [amount, setAmount] = useState("");
+    const [date, setDate] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if(!store || !amount || !date) return;
+
+
+        onAddReceipt( {store, amount, date} );
+
+        //Clearnign the store, amount, and date inputs in form:
+        setStore("");
+        setAmount("");
+        setDate("");
+    };
+
+    return(
+        <form onSubmit={handleSubmit}>
+            <input type="text" placeholder="Store Name" value={store} onChange={(e) => setStore(e.target.value)} />
+            <input type="number" placeholder="Amount" value={amount} onChange={(e) => setAmount(e.target.value)} />
+            <input type="date" value={date} onChange={(e) => setDate(e.target.value)} />
+            <button type="submit">Add Receipt</button>
+        </form>
+    );
+}
