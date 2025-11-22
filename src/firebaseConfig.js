00c63c44f420bb2c0a189d34b7e67345b2e7cb20
Abdulrahman Ali <abdul76985@gmail.com>
2025-11-22 01:50:06 -0500

    Fixed the import error.

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,7 +1,8 @@
-import { initializeApp, sendPasswordResetEmail } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider, sendPasswordResetEmail } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyCBoRdKRfE30fkM69RWTAT8sGrZlFbqRHA",
